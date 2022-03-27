d39457e3a0f7ee2b6b6315ddadcb95fff82e57e4
Bryan Dang <bryanvdang@gmail.com>
2022-03-26 17:11:09 -0700

    completed center header component by creating headerIcon component to display onClick and onHover

--- components/Header.js
+++ components/Header.js
@@ -14,31 +14,43 @@ import {
   SearchIcon,
   ShoppingCartIcon,
 } from "@heroicons/react/outline";
+import HeaderIcon from "./HeaderIcon";
 
 function Header() {
   return (
     <div>
-      {/* Header Left */}
-      <div className="flex items-center">
-        <Image
-          src="https://links.papareact.com/5me"
-          width={40}
-          height={40}
-          layout="fixed"
-          alt="fb logo"
-        />
-        <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
-          <SearchIcon className="h-6 text-gray-600" />
-          <input
-            className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500"
-            type="text"
-            placeholder="Search Facebook"
+      <div className={"flex items-center"}>
+        {/* Header Left */}
+        <div className="flex items-center">
+          <Image
+            src="https://links.papareact.com/5me"
+            width={40}
+            height={40}
+            layout="fixed"
+            alt="fb logo"
           />
+          <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
+            <SearchIcon className="h-6 text-gray-600" />
+            <input
+              className="flex ml-2 items-center bg-transparent outline-none 
+            placeholder-gray-500 flex-shrink"
+              type="text"
+              placeholder="Search Facebook"
+            />
+          </div>
         </div>
+        {/* Header Center */}
+        <div className="flex justify-center flex-grow">
+          <div className="flex space-x-6 md:space-x-2">
+            <HeaderIcon active Icon={HomeIcon} />
+            <HeaderIcon Icon={FlagIcon} />
+            <HeaderIcon Icon={PlayIcon} />
+            <HeaderIcon Icon={ShoppingCartIcon} />
+            <HeaderIcon Icon={UserGroupIcon} />
+          </div>
+        </div>
+        {/* Header Right */}
       </div>
-      {/* Header Center */}
-
-      {/* Header Right */}
     </div>
   );
 }
